6416c709e4bdd05416bcfcfee770c1bf2e8f48a6
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-05 14:05:44 +0200

    ITT: Fixture configured.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,4 +2,21 @@
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->setUpViews();
+        $this->setUpRoutes();
+    }
+
+    private function setUpViews()
+    {
+        app('view')->addLocation(__DIR__ . '/fixture/resources/views');
+    }
+
+    private function setUpRoutes()
+    {
+        app('router')->get('/', 'HomeController@index');
+    }
 }
