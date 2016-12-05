af7ed1e98b9d64cf8d16141ac0e77e9d66aadd6b
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-05 14:04:58 +0200

    ITT: Fixture templates added.

---
+++ tests/fixture/resources/views/layout/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Laravel Testing Tools</title>
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
