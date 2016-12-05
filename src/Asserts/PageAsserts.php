f571cb2b3af884100b38961ee04e6d2ff6b45059
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-05 14:54:47 +0200

    ITT: Page asserts added & covered by tests.

--- src/Asserts/PageAsserts.php
+++ src/Asserts/PageAsserts.php
@@ -6,7 +6,16 @@ trait PageAsserts
 {
     protected function seeElementTimes($selector, $times)
     {
-        $this->assertCount($times, $this->crawler->filter($selector));
+        $message = "Failed asserting that the page contains the element [{$selector}] {$times} times.";
+        $this->assertCount($times, $this->crawler->filter($selector), $message);
+
+        return $this;
+    }
+
+    protected function dontSeeElementTimes($selector, $times)
+    {
+        $message = "Failed asserting that the page not contains the element [{$selector}] {$times} times.";
+        $this->assertNotCount($times, $this->crawler->filter($selector), $message);
 
         return $this;
     }
--- tests/Asserts/PageAssertsTest.php
+++ tests/Asserts/PageAssertsTest.php
@@ -12,4 +12,14 @@ class PageAssertsTest extends TestCase
         $this->visit('/');
         $this->seeElementTimes('.body-item', 3);
     }
+
+    /** @test */
+    public function it_has_dont_see_element_times_assertion()
+    {
+        $this->visit('/');
+        $this->dontSeeElementTimes('.body-item', 1);
+        $this->dontSeeElementTimes('.body-item', 2);
+        $this->dontSeeElementTimes('.body-item', 4);
+        $this->dontSeeElementTimes('.body-item', 5);
+    }
 }
