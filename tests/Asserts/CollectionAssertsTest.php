85be3eb6dd553ffa46820958f1d5eec0574739fe
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-06 22:47:17 +0200

    ITT: CollectionAsserts tests added.

---
+++ tests/Asserts/CollectionAssertsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminated\Testing\Asserts\CollectionAsserts;
+
+class CollectionAssertsTest extends TestCase
+{
+    use CollectionAsserts;
+
+    /** @test */
+    public function it_has_equal_collections_assertion()
+    {
+        $collection1 = collect([
+            ['id' => 1, 'name' => 'John Doe'],
+            ['id' => 2, 'name' => 'Jane Doe'],
+            ['id' => 3, 'name' => 'Mary Doe'],
+        ]);
+
+        $collection2 = collect([
+            ['id' => 3, 'name' => 'Mary Doe'],
+            ['id' => 2, 'name' => 'Jane Doe'],
+            ['id' => 1, 'name' => 'John Doe'],
+        ]);
+
+        $this->assertEqualCollections($collection1, $collection2, 'id');
+    }
+}
