6f92a86a1a7b1e379523b0a7107b420de6ea1c84
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-08 19:05:28 +0200

    ITT: ScheduleAsserts added.

--- src/Asserts/ScheduleAsserts.php
+++ src/Asserts/ScheduleAsserts.php
@@ -2,6 +2,7 @@
 
 namespace Illuminated\Testing\Asserts;
 
+use Illuminate\Console\Scheduling\Event;
 use Illuminate\Console\Scheduling\Schedule;
 
 trait ScheduleAsserts
@@ -16,5 +17,33 @@ trait ScheduleAsserts
     {
         $message = "Failed asserting that schedule events count is not {$count}.";
         $this->assertNotCount($count, app(Schedule::class)->events(), $message);
+    }
+
+    protected function seeInSchedule($command, $expression, $runInBackground = false)
+    {
+        $event = $this->getScheduleEvent($command);
+
+        $message = "Failed asserting that command `{$command}` is in schedule.";
+        $this->assertNotEmpty($event, $message);
+        $this->assertInstanceOf(Event::class, $event, $message);
+
+        $message = "Failed asserting that command `{$command}` is in schedule as `{$expression}`.";
+        $this->assertEquals($expression, $event->expression, $message);
+
+        $message = "Failed asserting that command `{$command}` is scheduled with the same `run in background` mode.";
+        $this->assertEquals($runInBackground, $event->runInBackground, $message);
+    }
+
+    private function getScheduleEvent($command)
+    {
+        $schedule = app(Schedule::class);
+
+        foreach ($schedule->events() as $event) {
+            if (ends_with($event->command, $command)) {
+                return $event;
+            }
+        }
+
+        return false;
     }
 }
