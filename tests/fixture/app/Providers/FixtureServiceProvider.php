14ae9682f0e580ab68932aeac3ce32f7f621eecf
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-10 20:36:34 +0200

    ITT: Fixture service provider added.

---
+++ tests/fixture/app/Providers/FixtureServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Foundation\AliasLoader;
+
+class FixtureServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    public function register()
+    {
+        $this->registerAliases();
+        $this->registerCommands();
+    }
+
+    private function registerAliases()
+    {
+        $this->app->booting(function () {
+            $loader = AliasLoader::getInstance();
+            $loader->alias('Fixture\Alias\Post', Post::class);
+        });
+    }
+
+    private function registerCommands()
+    {
+        $this->app->singleton('command.fixture.generic', GenericCommand::class);
+
+        $this->commands(['command.fixture.generic']);
+    }
+}
