1978f52981520f58fe0c6a8fc1c34d20e7fdf1e6
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-13 21:29:31 +0200

    ITT: Trait asserts added.

---
+++ src/Asserts/TraitAsserts.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Illuminated\Testing\Asserts;
+
+trait TraitAsserts
+{
+    protected function assertTraitUsed($class, $trait)
+    {
+        $message = "Failed asserting that class `{$class}` is using trait `{$trait}`.";
+        $this->assertContains($trait, class_uses($class), $message);
+    }
+
+    protected function assertTraitNotUsed($class, $trait)
+    {
+        $message = "Failed asserting that class `{$class}` is not using trait `{$trait}`.";
+        $this->assertNotContains($trait, class_uses($class), $message);
+    }
+}
