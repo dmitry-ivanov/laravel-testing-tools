ee5ae02a2f5cb81214a706217b84703fea5b7608
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-13 22:31:15 +0200

    ITT: resolveApplicationConsoleKernel added.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Contracts\Console\Kernel as KernelContract;
+
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
     protected function setUp()
@@ -24,5 +26,12 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     private function setUpStorage()
     {
         $this->app->useStoragePath(__DIR__ . '/fixture/storage');
+    }
+
+    protected function resolveApplicationConsoleKernel($app)
+    {
+        $app->singleton(KernelContract::class, Kernel::class);
+
+        app(KernelContract::class);
     }
 }
