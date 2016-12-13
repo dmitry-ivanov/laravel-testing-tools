20e110ee2e0cc364692fc130d394d0952dd9ce32
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-13 22:33:40 +0200

    ITT: Artisan asserts fixed.

--- src/Asserts/ArtisanAsserts.php
+++ src/Asserts/ArtisanAsserts.php
@@ -8,13 +8,15 @@ trait ArtisanAsserts
 {
     protected function seeArtisanOutput($output)
     {
-        $output = trim($output);
-        $this->assertEquals($output, Artisan::output(), "Failed asserting that artisan output is `{$output}`.");
+        $expected = trim($output);
+        $actual = trim(Artisan::output());
+        $this->assertEquals($expected, $actual, "Failed asserting that artisan output is `{$expected}`.");
     }
 
     protected function dontSeeArtisanOutput($output)
     {
-        $output = trim($output);
-        $this->assertNotEquals($output, Artisan::output(), "Failed asserting that artisan output is not `{$output}`.");
+        $expected = trim($output);
+        $actual = trim(Artisan::output());
+        $this->assertNotEquals($expected, $actual, "Failed asserting that artisan output is not `{$expected}`.");
     }
 }
