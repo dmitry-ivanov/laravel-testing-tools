9a4a9b976d5fea3abeeccc1b96d3e3d1ebe45f3d
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-15 23:23:03 +0200

    ITT: ArtisanAsserts tests added.

--- tests/Asserts/ArtisanAssertsTest.php
+++ tests/Asserts/ArtisanAssertsTest.php
@@ -21,4 +21,16 @@ class ArtisanAssertsTest extends TestCase
 
         $this->dontSeeArtisanOutput('Hello, Universe!');
     }
+
+    /** @test */
+    public function it_has_see_artisan_table_output_assertion()
+    {
+        $this->artisan('table-output');
+
+        $this->seeArtisanTableOutput([
+            ['Date' => '2016-12-13 13:13:13', 'System' => 'Alpha', 'Status' => 'Enabled'],
+            ['Date' => '2016-12-14 14:14:14', 'System' => 'Beta', 'Status' => 'Enabled'],
+            ['Date' => '2016-12-15 15:15:15', 'System' => 'Gamma', 'Status' => 'Disabled'],
+        ]);
+    }
 }
