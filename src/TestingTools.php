b56a3728391c2fb7368a12251c70719c6330ecc3
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-15 18:38:16 +0200

    ITT: TestingTools trait added.

---
+++ src/TestingTools.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Illuminated\Testing;
+
+use Illuminated\Testing\Asserts\ArtisanAsserts;
+use Illuminated\Testing\Asserts\CollectionAsserts;
+use Illuminated\Testing\Asserts\DatabaseAsserts;
+use Illuminated\Testing\Asserts\ExceptionAsserts;
+use Illuminated\Testing\Asserts\LogFileAsserts;
+use Illuminated\Testing\Asserts\PageAsserts;
+use Illuminated\Testing\Asserts\ScheduleAsserts;
+use Illuminated\Testing\Asserts\ServiceProviderAsserts;
+use Illuminated\Testing\Asserts\TraitAsserts;
+
+trait TestingTools
+{
+    use EmulatesEnvironment;
+    use InteractsWithConsole;
+
+    use ArtisanAsserts;
+    use CollectionAsserts;
+    use DatabaseAsserts;
+    use ExceptionAsserts;
+    use LogFileAsserts;
+    use PageAsserts;
+    use ScheduleAsserts;
+    use ServiceProviderAsserts;
+    use TraitAsserts;
+}
