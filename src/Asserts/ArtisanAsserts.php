e4ab4bdbf82b9c602fe61c8b10542a84f7a42f5f
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-15 23:03:39 +0200

    ITT: `seeArtisanTableOutput` assertion added.

--- src/Asserts/ArtisanAsserts.php
+++ src/Asserts/ArtisanAsserts.php
@@ -19,4 +19,36 @@ trait ArtisanAsserts
         $actual = trim(Artisan::output());
         $this->assertNotEquals($expected, $actual, "Failed asserting that artisan output is not `{$expected}`.");
     }
+
+    protected function seeArtisanTableOutput(array $data)
+    {
+        $message = "Failed asserting that artisan table output equals to expected value.";
+        $this->assertEquals($data, $this->parseArtisanTableOutput(Artisan::output()), $message);
+    }
+
+    private function parseArtisanTableOutput($output)
+    {
+        $parsed = [];
+
+        $headers = [];
+        $outputRows = explode("\n", trim($output));
+        foreach ($outputRows as $row) {
+            if (!str_contains($row, '|')) {
+                continue;
+            }
+
+            $row = explode('|', $row);
+            $row = array_filter($row);
+            $row = array_map('trim', $row);
+
+            if (empty($headers)) {
+                $headers = $row;
+                continue;
+            }
+
+            $parsed[] = array_combine($headers, $row);
+        }
+
+        return $parsed;
+    }
 }
