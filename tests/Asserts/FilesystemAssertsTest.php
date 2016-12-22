1ef57e9fbc5a2a1ee508bd765222ede7bcc92d04
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-22 23:23:32 +0200

    ITT: FilesystemAsserts tests added.

--- tests/Asserts/FilesystemAssertsTest.php
+++ tests/Asserts/FilesystemAssertsTest.php
@@ -7,4 +7,11 @@ class FilesystemAssertsTest extends TestCase
     {
         $this->assertDirectoryEmpty(__DIR__ . '/FilesystemAssertsTest/Empty');
     }
+
+    /** @test */
+    public function it_has_directory_not_empty_assertion()
+    {
+        $this->assertDirectoryNotEmpty(__DIR__ . '/FilesystemAssertsTest/NotEmpty');
+        $this->assertDirectoryNotEmpty(__DIR__ . '/FilesystemAssertsTest/WithSubDirectory');
+    }
 }
