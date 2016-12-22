a37c7f179c855161b6ccbddaadac9a8a7cc90e3f
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-22 23:19:50 +0200

    ITT: FilesystemAsserts fixes.

--- src/Asserts/FilesystemAsserts.php
+++ src/Asserts/FilesystemAsserts.php
@@ -3,16 +3,19 @@
 namespace Illuminated\Testing\Asserts;
 
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 
 trait FilesystemAsserts
 {
     protected function assertDirectoryEmpty($path)
     {
-        $this->assertEmpty(File::glob($path), "Failed asserting that `{$path}` directory is empty.");
+        $glob = Str::finish($path, '/') . '*';
+        $this->assertEmpty(File::glob($glob), "Failed asserting that `{$path}` directory is empty.");
     }
 
     protected function assertDirectoryNotEmpty($path)
     {
-        $this->assertNotEmpty(File::glob($path), "Failed asserting that `{$path}` directory is not empty.");
+        $glob = Str::finish($path, '/') . '*';
+        $this->assertNotEmpty(File::glob($glob), "Failed asserting that `{$path}` directory is not empty.");
     }
 }
