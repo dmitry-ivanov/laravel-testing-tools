02b0a2496ddf8cfa7532a803c9fb405522974d15
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-22 23:44:22 +0200

    ITT: FilesystemAsserts assertions added.

--- src/Asserts/FilesystemAsserts.php
+++ src/Asserts/FilesystemAsserts.php
@@ -10,12 +10,24 @@ trait FilesystemAsserts
     protected function assertDirectoryEmpty($path)
     {
         $glob = Str::finish($path, '/') . '*';
-        $this->assertEmpty(File::glob($glob), "Failed asserting that `{$path}` directory is empty.");
+        $this->assertEmpty(File::glob($glob), "Failed asserting that directory `{$path}` is empty.");
     }
 
     protected function assertDirectoryNotEmpty($path)
     {
         $glob = Str::finish($path, '/') . '*';
-        $this->assertNotEmpty(File::glob($glob), "Failed asserting that `{$path}` directory is not empty.");
+        $this->assertNotEmpty(File::glob($glob), "Failed asserting that directory `{$path}` is not empty.");
+    }
+
+    protected function assertFilesCount($path, $count)
+    {
+        $message = "Failed asserting that directory `{$path}` has `{$count}` files.";
+        $this->assertCount($count, File::files($path), $message);
+    }
+
+    protected function assertNotFilesCount($path, $count)
+    {
+        $message = "Failed asserting that directory `{$path}` not has `{$count}` files.";
+        $this->assertNotCount($count, File::files($path), $message);
     }
 }
