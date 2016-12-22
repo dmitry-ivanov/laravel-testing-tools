9c033e71e475e6c2155a3d7f7296dbbd6f7e5358
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-22 14:27:09 +0200

    ITT: Tests improved.

--- tests/Helpers/InteractsWithConsoleTest.php
+++ tests/Helpers/InteractsWithConsoleTest.php
@@ -3,18 +3,20 @@
 class InteractsWithConsoleTest extends TestCase
 {
     /** @test */
-    public function it_can_run_console_command_by_class_name_via_object_and_return_it()
+    public function it_can_run_console_command_by_class_name_and_optional_parameters()
     {
-        $command = $this->runConsoleCommand(GenericCommand::class);
+        $command = $this->runConsoleCommand(GenericCommand::class, ['--name' => 'John']);
+
         $this->assertInstanceOf(GenericCommand::class, $command);
-        $this->assertEquals('Important!', $command->getSomethingImportant());
+        $this->assertEquals('Hello, John!', $command->getGreetingMessage());
     }
 
     /** @test */
-    public function it_can_run_console_command_by_passed_object_and_return_it()
+    public function it_can_run_console_command_by_object_and_optional_parameters()
     {
-        $command = $this->runConsoleCommand(new GenericCommand);
+        $command = $this->runConsoleCommand(new GenericCommand, ['--name' => 'Jane']);
+
         $this->assertInstanceOf(GenericCommand::class, $command);
-        $this->assertEquals('Important!', $command->getSomethingImportant());
+        $this->assertEquals('Hello, Jane!', $command->getGreetingMessage());
     }
 }
