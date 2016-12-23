7df530a55dd2005c3854c5990ac5a4f1416aed0b
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-23 23:25:34 +0200

    ITT: Custom latest artisan output logic.

--- src/Helpers/ArtisanHelpers.php
+++ src/Helpers/ArtisanHelpers.php
@@ -3,11 +3,14 @@
 namespace Illuminated\Testing\Helpers;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 
 trait ArtisanHelpers
 {
+    protected static $artisanOutput;
+
     protected function runArtisan($command, array $parameters = [])
     {
         if (!($command instanceof Command)) {
@@ -15,8 +18,22 @@ trait ArtisanHelpers
         }
 
         $command->setLaravel($this->app);
-        $command->run(new ArrayInput($parameters), new BufferedOutput);
+        $command->run(new ArrayInput($parameters), (self::$artisanOutput = new BufferedOutput));
 
         return $command;
+    }
+
+    protected function getArtisanOutput()
+    {
+        $output = Artisan::output();
+        if (!empty($output)) {
+            self::$artisanOutput = $output;
+        }
+
+        if (self::$artisanOutput instanceof BufferedOutput) {
+            self::$artisanOutput = self::$artisanOutput->fetch();
+        }
+
+        return self::$artisanOutput;
     }
 }
