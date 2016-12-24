f3f0375e3ff8faa6c4c230367bf59cc5e84e494c
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-24 18:51:19 +0200

    ITT: New database assertions added.

--- src/Asserts/DatabaseAsserts.php
+++ src/Asserts/DatabaseAsserts.php
@@ -2,8 +2,20 @@
 
 namespace Illuminated\Testing\Asserts;
 
+use Illuminate\Support\Facades\Schema;
+
 trait DatabaseAsserts
 {
+    protected function seeDatabaseTable($table)
+    {
+        $this->assertTrue(Schema::hasTable($table), "Failed asserting that database has table `{$table}`.");
+    }
+
+    protected function dontSeeDatabaseTable($table)
+    {
+        $this->assertFalse(Schema::hasTable($table), "Failed asserting that database not has table `{$table}`.");
+    }
+
     protected function seeInDatabaseMany($table, array $rows)
     {
         foreach ($rows as $row) {
