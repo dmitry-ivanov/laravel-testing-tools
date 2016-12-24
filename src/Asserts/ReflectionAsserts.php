3507898caab576d0047557487d6377bf43205f3b
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-24 17:18:29 +0200

    ITT: New reflection asserts added.

--- src/Asserts/ReflectionAsserts.php
+++ src/Asserts/ReflectionAsserts.php
@@ -4,6 +4,18 @@ namespace Illuminated\Testing\Asserts;
 
 trait ReflectionAsserts
 {
+    protected function assertSubclassOf($class, $parent)
+    {
+        $message = "Failed asserting that class `{$class}` is subclass of `{$parent}`.";
+        $this->assertTrue(is_subclass_of($class, $parent), $message);
+    }
+
+    protected function assertNotSubclassOf($class, $parent)
+    {
+        $message = "Failed asserting that class `{$class}` is not subclass of `{$parent}`.";
+        $this->assertFalse(is_subclass_of($class, $parent), $message);
+    }
+
     protected function assertTraitUsed($class, $trait)
     {
         $message = "Failed asserting that class `{$class}` is using trait `{$trait}`.";
