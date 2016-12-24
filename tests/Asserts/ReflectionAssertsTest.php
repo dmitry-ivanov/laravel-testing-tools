816d8979965fd33c5625e96f8c8b973422142b85
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-12-24 17:20:30 +0200

    ITT: New reflection asserts tests added.

--- tests/Asserts/ReflectionAssertsTest.php
+++ tests/Asserts/ReflectionAssertsTest.php
@@ -1,9 +1,23 @@
 <?php
 
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
 class ReflectionAssertsTest extends TestCase
 {
+    /** @test */
+    public function it_has_subclass_of_assertion()
+    {
+        $this->assertSubclassOf(Post::class, Model::class);
+    }
+
+    /** @test */
+    public function it_has_not_subclass_of_assertion()
+    {
+        $this->assertNotSubclassOf(Post::class, Command::class);
+    }
+
     /** @test */
     public function it_has_trait_used_assertion()
     {
