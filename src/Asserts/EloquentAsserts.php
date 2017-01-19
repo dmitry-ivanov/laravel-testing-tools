888b77012175353ebdf14988c0fa9a852b1f3ef3
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2017-01-19 19:43:04 +0200

    ITT: Eloquent asserts added.

--- src/Asserts/EloquentAsserts.php
+++ src/Asserts/EloquentAsserts.php
@@ -15,4 +15,16 @@ trait EloquentAsserts
         $message = "Failed asserting that Eloquent table not equals to `{$table}`.";
         $this->assertNotEquals($table, (new $class)->getTable(), $message);
     }
+
+    protected function assertEloquentFillableEquals($class, array $fillable)
+    {
+        $message = 'Failed asserting that Eloquent fillable equals to specified value.';
+        $this->assertEquals($fillable, (new $class)->getFillable(), $message);
+    }
+
+    protected function assertEloquentFillableNotEquals($class, array $fillable)
+    {
+        $message = 'Failed asserting that Eloquent fillable not equals to specified value.';
+        $this->assertNotEquals($fillable, (new $class)->getFillable(), $message);
+    }
 }
