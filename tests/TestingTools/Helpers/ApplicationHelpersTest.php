fb34b7f07584240e9481e39ee1973acb68e6c7e1
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2017-03-14 20:58:55 +0200

    ITT: Fixed according to recent changes.

--- tests/TestingTools/Helpers/ApplicationHelpersTest.php
+++ tests/TestingTools/Helpers/ApplicationHelpersTest.php
@@ -6,29 +6,34 @@ use Illuminated\Testing\TestingTools\Tests\TestCase;
 
 class ApplicationHelpersTest extends TestCase
 {
-    // /** @test */
-    // public function it_can_emulate_local_environment()
-    // {
-    //     $this->emulateLocal();
-    //
-    //     $this->assertEquals('local', $this->app->environment());
-    // }
-    //
-    // /** @test */
-    // public function it_can_emulate_production_environment()
-    // {
-    //     $this->emulateProduction();
-    //
-    //     $this->assertEquals('production', $this->app->environment());
-    // }
-    //
-    // /** @test */
-    // public function it_can_emulate_any_environment()
-    // {
-    //     $this->emulateEnvironment('demo');
-    //
-    //     $this->assertEquals('demo', $this->app->environment());
-    // }
+    protected function getPackageProviders($app)
+    {
+        return [];
+    }
+
+    /** @test */
+    public function it_can_emulate_local_environment()
+    {
+        $this->emulateLocal();
+
+        $this->assertEquals('local', $this->app->environment());
+    }
+
+    /** @test */
+    public function it_can_emulate_production_environment()
+    {
+        $this->emulateProduction();
+
+        $this->assertEquals('production', $this->app->environment());
+    }
+
+    /** @test */
+    public function it_can_emulate_any_environment()
+    {
+        $this->emulateEnvironment('demo');
+
+        $this->assertEquals('demo', $this->app->environment());
+    }
 
     /** @test */
     public function it_can_check_if_tests_are_running_on_travis()
