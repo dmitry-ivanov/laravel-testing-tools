13332301c6cf265ea2787d827219497c34be66cb
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2017-03-15 11:56:07 +0200

    ITT: Collections asserts refactored.

--- src/Asserts/CollectionAsserts.php
+++ src/Asserts/CollectionAsserts.php
@@ -8,19 +8,25 @@ trait CollectionAsserts
 {
     protected function assertCollectionsEqual(Collection $collection1, Collection $collection2, $key)
     {
-        $collection1 = $collection1->pluck($key);
-        $collection2 = $collection2->pluck($key);
-        $diff = $collection1->diff($collection2);
+        $keys1 = $collection1->pluck($key)->toArray();
+        $keys2 = $collection2->pluck($key)->toArray();
 
-        $this->assertEmpty($diff, 'Failed asserting that collections are equal.');
+        $diff1 = array_diff($keys1, $keys2);
+        $diff2 = array_diff($keys2, $keys1);
+
+        $bothDiffsAreEmpty = (empty($diff1) && empty($diff2));
+        $this->assertTrue($bothDiffsAreEmpty, 'Failed asserting that collections are equal.');
     }
 
     protected function assertCollectionsNotEqual(Collection $collection1, Collection $collection2, $key)
     {
-        $collection1 = $collection1->pluck($key);
-        $collection2 = $collection2->pluck($key);
-        $diff = $collection1->diff($collection2);
+        $keys1 = $collection1->pluck($key)->toArray();
+        $keys2 = $collection2->pluck($key)->toArray();
 
-        $this->assertNotEmpty($diff, 'Failed asserting that collections are not equal.');
+        $diff1 = array_diff($keys1, $keys2);
+        $diff2 = array_diff($keys2, $keys1);
+
+        $atLeastOneDiffIsNotEmpty = (!empty($diff1) || !empty($diff2));
+        $this->assertTrue($atLeastOneDiffIsNotEmpty, 'Failed asserting that collections are not equal.');
     }
 }
