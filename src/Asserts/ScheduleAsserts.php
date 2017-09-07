3dbc5f389f0de639e6b1bf5acd97ba65df37b060
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2017-09-07 10:22:09 +0300

    ITT: Laravel 5.2 support.

--- src/Asserts/ScheduleAsserts.php
+++ src/Asserts/ScheduleAsserts.php
@@ -31,11 +31,8 @@ trait ScheduleAsserts
         $expression = $this->normalizeScheduleExpression(clone $event, $expression);
         $this->assertEquals($expression, $event->expression, $message);
 
-        /* @laravel-versions */
-        if (isset($event->runInBackground)) {
-            $message = "Failed asserting that command `{$command}` is scheduled with the same `run in background` mode.";
-            $this->assertEquals($runInBackground, $event->runInBackground, $message);
-        }
+        $message = "Failed asserting that command `{$command}` is scheduled with the same `run in background` mode.";
+        $this->assertEquals($runInBackground, $event->runInBackground, $message);
     }
 
     protected function dontSeeInSchedule($command)
